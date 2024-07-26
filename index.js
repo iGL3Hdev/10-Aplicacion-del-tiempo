b00f2accbf3de9203df9e3387ed4ec8b594855f3
Carlos Iglesias <igl3hdev@gmail.com>
2024-07-26 17:26:56 +0200

    Inicio JS1

---
+++ index.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
+
+    const APIKey = 'Your Api Key';
+    const city = document.querySelector('.search-box input').value;
+
+    if (city === '')
+        return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(json => {
+            
+        })
+})

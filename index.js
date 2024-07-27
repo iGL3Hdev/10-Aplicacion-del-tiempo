44a6246e170ab372f942daf220dc9e8034bea7ea
Carlos Iglesias <igl3hdev@gmail.com>
2024-07-27 12:26:06 +0200

    js final

--- index.js
+++ index.js
@@ -16,5 +16,58 @@ search.addEventListener('click', () => {
         .then(response => response.json())
         .then(json => {
             
-        })
-})
+            if (json.cod === '404') {
+                container.style.height = '400px';
+                weather.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return,
+            }
+
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
+
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
+
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    img.src = 'images/clear.png'
+                    break;
+            
+                case 'Rain':
+                    img.src = 'images/rain.png'
+                    break;
+
+                case 'Snow':
+                    image.src =  'images/snow.png'
+                    break;
+                
+                case 'Clouds':
+                    image.src = 'images/cloud.png'
+                    break;
+                
+                case 'Haze':
+                    image.src = 'images/mist.org'
+                    break;
+
+                default:
+                    image.src = '';
+            }
+
+            temperature.innerHTML = '${parseInt(json.main.temp)}<span> ºC </span>';
+            description.innerHTML = '${json.weather[0].description}';
+            humidity.innerHTML = '${json.main.humidity}%';
+            wind.innerHTML = '${parseInt(json.wind.spedd)km/h';
+
+            weatherBox.style.display = '';
+            weatherDetails.style.display = ''; 
+            weatherBox.classList.add('fadeIn');
+            weather.classList.add('fadeIn') :
+            containter.style.height = '590px'
+        });
+});
